19a49837c235086febc57db482195337671c24c6
Ayo <ramon.aycojr@gmail.com>
2022-09-25 21:33:19 +0200

    feat: multiple controls

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-// Do not write code directly here, instead use the `src` folder!
-
-// What you should do here is re-exports all the things you want your user to access, ex:
-// export { HelloWorld } from "./src/main.ts"
-// export type { HelloWorldResult } from "./src/types.ts"
 import Form from './src/Form.astro';
 export default Form;
-export {FormGroup, FormControl} from './src/index';
+export * from './src/index';
