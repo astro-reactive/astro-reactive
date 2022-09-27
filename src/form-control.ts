d585975091ae221f4a0e9fc0f3850a86b7b5ac43
Ayo <ramon.aycojr@gmail.com>
2022-09-27 17:37:32 +0200

    feat: Checkbox and Radio classes, todo comment

--- src/form-control.ts
+++ src/form-control.ts
@@ -1,7 +1,21 @@
 export class FormControl {
 	name: string;
-  type?: 'text' | 'checkbox' | 'radio' = 'text'; // add more
-  value?: string | number | null | string[];
+	type?: 'text' | 'checkbox' | 'radio' = 'text'; // add more
+	value?: string | number | null | string[];
 	label?: string;
 	labelPosition?: 'right' | 'left' = 'left';
+}
+
+/**
+ * TODO: Create classes for each control type
+ */
+
+export class Checkbox extends FormControl {
+	type: 'checkbox';
+	checked: boolean;
+}
+
+export class Radio extends FormControl {
+	type: 'checkbox';
+	checked: boolean;
 }
