0968ca657dbb03ea18d7713e5460e72a579d8f49
Ayo <ramon.aycojr@gmail.com>
2022-10-14 20:13:12 +0200

    test: update tests for validator

---
+++ packages/validator/test/Validator.astro.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { describe, beforeEach, it } from 'mocha';
+import { getComponentOutput } from 'astro-component-tester';
+
+describe('Example Tests', () => {
+	let component;
+
+	beforeEach(() => {
+		component = undefined;
+	});
+
+	describe('Component test', async () => {
+		it('example component should be empty', async () => {
+			component = await getComponentOutput('./Validator.astro');
+			expect(component.html).not.to.equal('\n');
+		});
+	});
+});
