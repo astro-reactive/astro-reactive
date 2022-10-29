9bdf8c0fe0cd03398c6277af2dcf783465efbc4d
Ayo <ramon.aycojr@gmail.com>
2022-10-29 21:17:04 +0200

    feat(demo): new nav component

--- apps/demo/astro.config.mjs
+++ apps/demo/astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-    integrations: [tailwind()]
-});
+export default defineConfig({});
