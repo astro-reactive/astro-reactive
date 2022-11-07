e6ca1f85bd805283bc71a44b69bd73c2587c557b
Ayo <ramon.aycojr@gmail.com>
2022-11-07 10:58:58 +0100

    Docs: update community invite link

--- apps/docs/src/config.ts
+++ apps/docs/src/config.ts
@@ -33,7 +33,7 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/ayoayco/astro-reactive-library/tree/main/apps/docs`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://github.com/ayoayco/astro-reactive-library/discussions`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
