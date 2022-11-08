93a8d49f0a2b96164c04bc03dc6cd473c63ceb69
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2022-11-08 02:53:22 -0500

    feat(form): implement unique IDs (#182)
    * Added Short Unique ID npm library
    
    * Added unique ID to form-control
    
    * Added unique ID to form-group
    
    * Unique ID added to <form>
    
    * Added unique IDs to control components
    
    * Added IDs to label and fieldset
    
    * Update Form.astro with requested changes
    
    Co-authored-by: Ayo Ayco <ayo@ayco.io>
    
    * Adjustments for requested changes.
    
    Co-authored-by: Ayo Ayco <ayo@ayco.io>

--- packages/form/core/form-group.ts
+++ packages/form/core/form-group.ts
@@ -1,11 +1,15 @@
 import { ControlConfig, FormControl } from './form-control';
+import ShortUniqueId from 'short-unique-id';
 
 export class FormGroup {
 	controls: FormControl[];
 	name?: string;
+	id?: string;
 
 	constructor(controls: ControlConfig[], name = '') {
+		const uid = new ShortUniqueId({ length: 9 });
 		this.name = name;
+		this.id = 'arl-' + uid();
 		this.controls = controls
 			.filter((control) => control.type !== 'submit')
 			.map((control) => new FormControl(control));
