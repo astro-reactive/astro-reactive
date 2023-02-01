f05026c453606aaa85b567dbc52fc71ab3475491
Ayo <ramon.aycojr@gmail.com>
2023-02-01 18:00:01 +0100

    devops: bring back form test CI workaround

--- packages/form/vitest.config.ts
+++ packages/form/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		testTimeout: 30000,
+		threads: !process.env.CI,
 		coverage: {
 			provider: 'istanbul',
 		},
