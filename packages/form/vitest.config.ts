9aaf9984ef9e5d88bb0b4b32f0d4f051a3cd0c57
Ayo <ramon.aycojr@gmail.com>
2023-02-01 17:57:19 +0100

    chore: set up test:coverage scripts

--- packages/form/vitest.config.ts
+++ packages/form/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		testTimeout: 30000,
-		threads: !process.env.CI,
+		coverage: {
+			provider: 'istanbul',
+		},
 	},
 });
