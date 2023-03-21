50617941677bd6d59410755f6c41f5a9c15e106e
fazzaamiarso <fazzaamiarso@gmail.com>
2023-03-22 00:46:24 +0700

    fix: prerender on root?

--- apps/playground/src/routes/+page.ts
+++ apps/playground/src/routes/+page.ts
@@ -1,8 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const prerender = true;
-
 export const load = (() => {
 	throw redirect(307, '/playground');
 }) satisfies PageLoad;
