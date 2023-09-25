929f3a1e69016867ae6219467097c23eb0644595
Ayo <ramon.aycojr@gmail.com>
2023-09-25 17:01:49 +0200

    chore: remove netlify

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,8 +1,7 @@
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	integrations: [tailwind(), netlify()],
+	integrations: [tailwind()],
 	site: 'https://astro-reactive.dev',
 });
