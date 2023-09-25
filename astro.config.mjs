29c25dde874e7b046447637d6fc28183615be31f
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:54:03 +0200

    chore: use netlify()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
