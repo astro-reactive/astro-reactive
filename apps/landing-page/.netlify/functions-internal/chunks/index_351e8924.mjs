e85bf59b0f3dce9557352a0330920fc5c93a2bde
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:45:30 +0200

    chore: remove adapter

--- apps/landing-page/.netlify/functions-internal/chunks/index_351e8924.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_b8dca542.mjs');
-
-export { page };
--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,10 +1,7 @@
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://astro-reactive.dev',
 	integrations: [tailwind()],
-	output: 'server',
-	adapter: netlify(),
 });
