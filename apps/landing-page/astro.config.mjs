6f608e470e1d1d4712601661a310f310870d0895
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:47:28 +0200

    chore: clean up integrations

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -2,6 +2,6 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://astro-reactive.dev',
 	integrations: [tailwind()],
+	site: 'https://astro-reactive.dev',
 });
