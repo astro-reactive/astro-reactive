78b9ae07cfd96acbf7db2f14194026cd8ef6c712
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:37:20 +0200

    chore: integrate netlify

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://astro-reactive.dev',
 	integrations: [tailwind()],
+	output: 'server',
+	adapter: netlify(),
 	vite: {
 		ssr: {
 			external: ['svgo'],
