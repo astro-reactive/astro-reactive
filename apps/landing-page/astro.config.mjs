4bd37804a933438a40a57af9fa44f34eca88dcf4
Ayo <ramon.aycojr@gmail.com>
2023-09-25 17:13:43 +0200

    chore: update package-lock

--- apps/landing-page/astro.config.mjs
+++ apps/landing-page/astro.config.mjs
@@ -1,5 +1,5 @@
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
 	integrations: [tailwind()],
