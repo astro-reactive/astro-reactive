5ad276cbf9dcac359dcac64f7d3584146cd2aa18
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:49:14 +0200

    chore: add netlify integration

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify()
+});
