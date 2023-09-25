633f94ce2522f9c5ef9e85d444c825be7dfddc1a
Ayo <ramon.aycojr@gmail.com>
2023-09-25 16:43:32 +0200

    chore: remove unused functions

---
+++ apps/landing-page/.netlify/functions-internal/chunks/image-endpoint_6e91a302.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/image-endpoint_fb876144.mjs');
+
+export { page };
---
+++ apps/landing-page/.netlify/functions-internal/chunks/index_351e8924.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_b8dca542.mjs');
+
+export { page };
