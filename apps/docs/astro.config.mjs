a1cc4b006be09da6dec1b2ddf064b3e75553a568
Ayo Ayco <ramon.aycojr@gmail.com>
2023-11-11 20:48:52 +0100

    chore: update homepage to astro-reactive.js.org (#309)

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://docs.astro-reactive.dev`,
+	site: `https://astro-reactive-docs.netlify.app`,
 });
